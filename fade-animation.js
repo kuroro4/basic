e1558983845b7661d061739e78a4b0c4844b61ec
kuroro4 <106098699+kuroro4@users.noreply.github.com>
2022-09-10 03:41:50 +0900

    Update fade-animation.js

--- fade-animation.js
+++ fade-animation.js
@@ -1,4 +1,4 @@
-const img = ["./images/1275782831_Starry_sky_landscape_painting_Ghibli_style.png", "./images/1888670209_Starry_sky_landscape_painting_Ghibli_style.png", "./images/2126285020_Starry_sky_landscape_painting_Ghibli_style.png", "./images/3628760897_Starry_sky_landscape_painting_Ghibli_style.png"];
+const img = ["https://github.com/kuroro4/basic/blob/main/1275782831_Starry_sky_landscape_painting_Ghibli_style.png?raw=true", "https://github.com/kuroro4/basic/blob/main/1888670209_Starry_sky_landscape_painting_Ghibli_style.png?raw=true", "https://github.com/kuroro4/basic/blob/main/2126285020_Starry_sky_landscape_painting_Ghibli_style.png?raw=true", "https://github.com/kuroro4/basic/blob/main/3628760897_Starry_sky_landscape_painting_Ghibli_style.png?raw=true"];
 let count = -1;
 picChange();
 function picChange() {
